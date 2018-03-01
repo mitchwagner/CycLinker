67d5570c2b41a2a081fc3c3613ae46d15dced730
Mitchell Wagner <mitchw94@vt.edu>
2018-02-28 19:34:58 -0500

    rm duplicate code to compile, and replace tabs

--- src/main/java/AlgorithmRLCSP.java
+++ src/main/java/AlgorithmRLCSP.java
@@ -8,36 +8,32 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-// TODO: This is not done yet. Needs:
-// 1) Debugging
-// 2) Sorting edge length for each corresponding edge and outputting
-
 public class AlgorithmRLCSP {
-	// These hashmaps map strings for proteins like "P04355" to integers, and
-	// vice versa
-	// Overflow warning: These integers should never be multiplied, without
-	// converting to longs.
-	HashMap<String, Integer> mapToInt;
-	HashMap<Integer, String> reverseMap;
+    // These hashmaps map strings for proteins like "P04355" to integers, and
+    // vice versa
+    // Overflow warning: These integers should never be multiplied, without
+    // converting to longs.
+    HashMap<String, Integer> mapToInt;
+    HashMap<Integer, String> reverseMap;
 
-	// Map of edge number to the edge start point and end point, as well as
-	// cost. (order of file read)
-	ArrayList<Integer> edgeEndSet;
-	ArrayList<Integer> edgeStartSet;
-	ArrayList<Double> weights;
-	ArrayList<Edge>[] edges;
-	ArrayList<Edge>[] reverseEdges;
+    // Map of edge number to the edge start point and end point, as well as
+    // cost. (order of file read)
+    ArrayList<Integer> edgeEndSet;
+    ArrayList<Integer> edgeStartSet;
+    ArrayList<Double> weights;
+    ArrayList<Edge>[] edges;
+    ArrayList<Edge>[] reverseEdges;
 
-	// Negative log transform map of edge costs.
-	// Key for edge is a perfect hash of the start and end point. (use given
-	// function. Works for up to 999,999,999 nodes)
-	HashMap<Long, Double> edgeCost;
+    // Negative log transform map of edge costs.
+    // Key for edge is a perfect hash of the start and end point. (use given
+    // function. Works for up to 999,999,999 nodes)
+    HashMap<Long, Double> edgeCost;
 
-	// Used internally in dijkstra's, and in CriticalPath for path printing.
-	// Stores the dijkstra's path in symbolic tree form.
-	int[] pathForward;
-	int[] pathBackward;
-	final long INF = Long.MAX_VALUE;
+    // Used internally in dijkstra's, and in CriticalPath for path printing.
+    // Stores the dijkstra's path in symbolic tree form.
+    int[] pathForward;
+    int[] pathBackward;
+    final long INF = Long.MAX_VALUE;
 
 
     // For projecting back to the original graph
@@ -45,35 +41,35 @@ public class AlgorithmRLCSP {
     HashMap<String, Integer> networkNodeToInt;
     HashMap<Integer, String> networkIntToNode;
     
-	HashMap<Long, ArrayList<Long>> correspondingEdges;
-	HashMap<Long, Long> correspondingEdgesReverse;
+    HashMap<Long, ArrayList<Long>> correspondingEdges;
+    HashMap<Long, Long> correspondingEdgesReverse;
 
-	// Stores the output to pass to the outputWriter class.
-	StringBuilder pathOutput;
-	StringBuilder edgeOutput;
-	StringBuilder correspondingEdgeOutput;
+    // Stores the output to pass to the outputWriter class.
+    StringBuilder pathOutput;
+    StringBuilder edgeOutput;
+    StringBuilder correspondingEdgeOutput;
 
     //maximum number of paths to write
     long maxk;
     long threshold;
 
-	public AlgorithmRLCSP(InputReaderRLCSP graph, long maxK, long threshold) {
+    public AlgorithmRLCSP(InputReaderRLCSP graph, long maxK, long threshold) {
         this.threshold = threshold;
-	    
-		// set up our variables
-		mapToInt = graph.productNodeToInt;
-		reverseMap = graph.productIntToNode;
 
-		edgeEndSet = graph.productEdgeEndSet;
-		edgeStartSet = graph.productEdgeStartSet;
-		weights = graph.productWeights;
-		edges = graph.productEdges;
-		reverseEdges = graph.productReverseEdges;
+        //set up our variables
+        mapToInt = graph.productNodeToInt;
+        reverseMap = graph.productIntToNode;
 
-		edgeCost = graph.productEdgeCost;
-		pathOutput = new StringBuilder();
-		edgeOutput = new StringBuilder();
-		correspondingEdgeOutput = new StringBuilder();
+        edgeEndSet = graph.productEdgeEndSet;
+        edgeStartSet = graph.productEdgeStartSet;
+        weights = graph.productWeights;
+        edges = graph.productEdges;
+        reverseEdges = graph.productReverseEdges;
+
+        edgeCost = graph.productEdgeCost;
+        pathOutput = new StringBuilder();
+        edgeOutput = new StringBuilder();
+        correspondingEdgeOutput = new StringBuilder();
 
         this.correspondingEdges =
             graph.correspondingEdges;
@@ -85,45 +81,45 @@ public class AlgorithmRLCSP {
         networkIntToNode = graph.networkIntToNode;
 
         maxk = maxK;
-	}
+    }
 
-	public ArrayList<EdgeRLCSP<String>> readEdgesToCompute(File f) 
-	        throws FileNotFoundException {
-	    ArrayList<EdgeRLCSP<String>> list = new ArrayList<EdgeRLCSP<String>>();
-	    Scanner scanner = new Scanner(f);
+    public ArrayList<EdgeRLCSP<String>> readEdgesToCompute(File f) 
+            throws FileNotFoundException {
+        ArrayList<EdgeRLCSP<String>> list = new ArrayList<EdgeRLCSP<String>>();
+        Scanner scanner = new Scanner(f);
 
-	    while (scanner.hasNext()) {
-	        String tail = scanner.next();
-	        String head = scanner.next();
-	        EdgeRLCSP<String> edge = new EdgeRLCSP<String>(tail, head);
-	        list.add(edge);
-	        scanner.nextLine();
-	    }
+        while (scanner.hasNext()) {
+            String tail = scanner.next();
+            String head = scanner.next();
+            EdgeRLCSP<String> edge = new EdgeRLCSP<String>(tail, head);
+            list.add(edge);
+            scanner.nextLine();
+        }
 
-	    return list;
-	}
+        return list;
+    }
 
-	public void run(String edgesToComputeFile) throws FileNotFoundException {
+    public void run(String edgesToComputeFile) throws FileNotFoundException {
 
-	    System.out.println(edgesToComputeFile);
+        System.out.println(edgesToComputeFile);
 
-		int start = 0; // super source = 0
-		int end = 1; // super sink = 1
-		int n = mapToInt.size(); // number of nodes
-		int e = weights.size(); // number of edges
+        int start = 0; // super source = 0
+        int end = 1; // super sink = 1
+        int n = mapToInt.size(); // number of nodes
+        int e = weights.size(); // number of edges
 
-		// Dijkstra's from the start, and the end.
-		// Stores the cost at from the start to all points.
-		final double[] startFromAllNodes = dijkstraFront(n, start, edges);
+        // Dijkstra's from the start, and the end.
+        // Stores the cost at from the start to all points.
+        final double[] startFromAllNodes = dijkstraFront(n, start, edges);
 
-		// Stores the cost from the end to all points.
-		final double[] endFromAllNodes = dijkstra(n, end, reverseEdges);
+        // Stores the cost from the end to all points.
+        final double[] endFromAllNodes = dijkstra(n, end, reverseEdges);
 
-		// Each edge has a shortest path; We will denote this as a
-		// 'CriticalPath'.
-		// A critical path does not contain only redundant edges. However, a
-		// CriticalPath might be the same as another CriticalPath.
-		TreeSet<CriticalPath> potentialPaths = new TreeSet<CriticalPath>();
+        // Each edge has a shortest path; We will denote this as a
+        // 'CriticalPath'.
+        // A critical path does not contain only redundant edges. However, a
+        // CriticalPath might be the same as another CriticalPath.
+        TreeSet<CriticalPath> potentialPaths = new TreeSet<CriticalPath>();
 
         
         if (edgesToComputeFile == "") {
@@ -165,12 +161,6 @@ public class AlgorithmRLCSP {
                             startFromAllNodes, 
                             endFromAllNodes,
                             edges);
-                        CriticalPath tempPath = new CriticalPath(
-                            edgeStartSet.get(a),
-                            edgeEndSet.get(a), 
-                            startFromAllNodes, 
-                            endFromAllNodes,
-                            edges);
 
                         potentialPaths.add(tempPath);
                         
@@ -188,35 +178,35 @@ public class AlgorithmRLCSP {
 
 
 
-		ArrayList<CriticalPath> outputPaths = new ArrayList<CriticalPath>();
+        ArrayList<CriticalPath> outputPaths = new ArrayList<CriticalPath>();
 
-		// This was in pathlinker output so I do it as well
-		edgeOutput.append("# Tail	Head	KSP Index	Path Cost\n"); 
+        // This was in pathlinker output so I do it as well
+        edgeOutput.append("# Tail\tHead\tKSP\tIndex\tPath Cost\n"); 
 
-		int count2 = 0;
-		int countPath = 0;
+        int count2 = 0;
+        int countPath = 0;
 
-		// This hashmap stores a blacklist of edges that have appeared on
-		// earlier paths
-		HashSet<Long> ReWriteThisWithEdgeClassLater = new HashSet<Long>();
+        // This hashmap stores a blacklist of edges that have appeared on
+        // earlier paths
+        HashSet<Long> ReWriteThisWithEdgeClassLater = new HashSet<Long>();
 
-		HashSet<Long> correspondingEdgeBlacklist = new HashSet<Long>();
+        HashSet<Long> correspondingEdgeBlacklist = new HashSet<Long>();
 
         double lastcost = 0;
         long rank = 0;
-		// Go through all of the potential paths.
-		while (!potentialPaths.isEmpty()) {
-			count2++;
-			CriticalPath get = potentialPaths.first();
-			potentialPaths.remove(get);
-			
-			// Start node and end node here define an edge, not 
-			// for the path.
-			long startNode = get.startNode;
-			long endNode = get.endNode;
-			outputPaths.add(get);
+        // Go through all of the potential paths.
+        while (!potentialPaths.isEmpty()) {
+            count2++;
+            CriticalPath get = potentialPaths.first();
+            potentialPaths.remove(get);
 
-			if (get.totalCost < this.threshold) {
+            // Start node and end node here define an edge, not 
+            // for the path.
+            long startNode = get.startNode;
+            long endNode = get.endNode;
+            outputPaths.add(get);
+
+            if (get.totalCost < this.threshold) {
 
                 // Output the edge
                 if (startNode != 0 && endNode != 1 && countPath < maxk) {
@@ -288,10 +278,10 @@ public class AlgorithmRLCSP {
                 }
             }
         }
-	}
+    }
 
-	// I don't really want to change the output for the ranked paths, but I 
-	// want to change or supplement the output for the ranked edges. 
+    // I don't really want to change the output for the ranked paths, but I 
+    // want to change or supplement the output for the ranked edges. 
 
     // Rather than output a1, b2, etc., I just want to output a, b, etc.
     // When I pull an edge ((u, x), (v, y)) from the edgeset, it is guaranteed
@@ -318,30 +308,30 @@ public class AlgorithmRLCSP {
     // but that output will just correspond to the shortest paths for H, not
     // for G.
 
-	// Converts a list of node ID's to a list of node names.
-	// Ex: [123, 4123] -> "P03422|Q02312"
-	private static String getString(ArrayList<Integer> path,
-			HashMap<Integer, String> reverseMap) {
-		StringBuilder node_names = new StringBuilder();
-		for (Integer a : path) {
-			if (a != 0 && a != 1) {
+    // Converts a list of node ID's to a list of node names.
+    // Ex: [123, 4123] -> "P03422|Q02312"
+    private static String getString(ArrayList<Integer> path,
+            HashMap<Integer, String> reverseMap) {
+        StringBuilder node_names = new StringBuilder();
+        for (Integer a : path) {
+            if (a != 0 && a != 1) {
                 node_names.append(reverseMap.get(a) + "|");
             }
-		}
+        }
         // remove the last '|'
         String output = node_names.toString();
         output = output.substring(0, output.length()-1);
-		return output;
-	}
+        return output;
+    }
 
     // Returns a unique 64-bit integer representation of an edge. (hash with no
     // collisions)
-	// No two edges can share the same representation.
-	private static long hash(long startNode, long endNode) {
-		// As long as endNode is below 1 billion and above or equal to 0, this
-		// function will never have a collision
-		return startNode * 1000000000l + endNode;
-	}
+    // No two edges can share the same representation.
+    private static long hash(long startNode, long endNode) {
+        // As long as endNode is below 1 billion and above or equal to 0, this
+        // function will never have a collision
+        return startNode * 1000000000l + endNode;
+    }
 
 	class CriticalPath implements Comparable<CriticalPath> {
 		// Start node of the edge defining the 'CriticalPath'
@@ -409,37 +399,37 @@ public class AlgorithmRLCSP {
 		}
 	}
 
-	// /////////////////////////////////////////////////////////////////////////
-	// Anything below here is a slightly modified dijkstras SSAD //
-	// ////////////////////////////////////////////////////////////////////////
-	static class VertexDist implements Comparable<VertexDist> {
-		private static final double EPS = 1E-14;
-		int vertex;
-		double distance;
+    // /////////////////////////////////////////////////////////////////////////
+    // Anything below here is a slightly modified dijkstras SSAD //
+    // ////////////////////////////////////////////////////////////////////////
+    static class VertexDist implements Comparable<VertexDist> {
+        private static final double EPS = 1E-14;
+        int vertex;
+        double distance;
 
-		public VertexDist(int vertex, double distance) {
-			this.vertex = vertex;
-			this.distance = distance;
-		}
+        public VertexDist(int vertex, double distance) {
+            this.vertex = vertex;
+            this.distance = distance;
+        }
 
-		public String toString() {
-			return "(" + vertex + " " + distance + ")";
-		}
+        public String toString() {
+            return "(" + vertex + " " + distance + ")";
+        }
 
-		@Override
-		public int compareTo(VertexDist arg0) {
-			VertexDist temp = arg0;
-			if (Math.abs(distance - temp.distance) < EPS) {
-				return vertex - temp.vertex;
-			}
-			if (distance < temp.distance) {
-				return -1;
-			} else {
-				return 1;
-			}
-		}
+        @Override
+        public int compareTo(VertexDist arg0) {
+            VertexDist temp = arg0;
+            if (Math.abs(distance - temp.distance) < EPS) {
+                return vertex - temp.vertex;
+            }
+            if (distance < temp.distance) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
 
-	}
+    }
 
 	/*
 	 * Extensively modified.
@@ -481,43 +471,43 @@ public class AlgorithmRLCSP {
 		// 'dist' contains the shortest distance from start to all nodes
 	}
 
-	/*
-	 * Extensively modified.
-	 * 
-	 * @author Godmar Back
-	 */
-	double[] dijkstraFront(int N, int start, ArrayList<Edge>[] edges) {
-		final double[] dist = new double[N];
-		pathForward = new int[N];
-		Arrays.fill(pathForward, -3);
+    /*
+     * Extensively modified.
+     * 
+     * @author Godmar Back
+     */
+    double[] dijkstraFront(int N, int start, ArrayList<Edge>[] edges) {
+        final double[] dist = new double[N];
+        pathForward = new int[N];
+        Arrays.fill(pathForward, -3);
 
-		Arrays.fill(dist, INF);
-		dist[start] = 0;
-		pathForward[start] = -1;
-		TreeSet<VertexDist> frontier = new TreeSet<VertexDist>();
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+        pathForward[start] = -1;
+        TreeSet<VertexDist> frontier = new TreeSet<VertexDist>();
 
-		frontier.add(new VertexDist(start, 0));
-		boolean[] optimal = new boolean[N];
-		while (!frontier.isEmpty()) {
-			VertexDist u2 = frontier.pollFirst();
-			int u = u2.vertex;
-			if (optimal[u])
-				continue;
+        frontier.add(new VertexDist(start, 0));
+        boolean[] optimal = new boolean[N];
+        while (!frontier.isEmpty()) {
+            VertexDist u2 = frontier.pollFirst();
+            int u = u2.vertex;
+            if (optimal[u])
+                continue;
 
-			optimal[u] = true;
-			for (Edge e : edges[u]) {
-				double uv = e.dist;
-				int v = e.end;
-				if (uv != INF) {
-					if (dist[u] + uv < dist[v]) {
-						dist[v] = dist[u] + uv;
-						pathForward[v] = u;
-						frontier.add(new VertexDist(v, dist[v]));
-					}
-				}
-			}
-		}
-		return dist;
-		// 'dist' contains the shortest distance from start to all nodes
-	}
+            optimal[u] = true;
+            for (Edge e : edges[u]) {
+                double uv = e.dist;
+                int v = e.end;
+                if (uv != INF) {
+                    if (dist[u] + uv < dist[v]) {
+                        dist[v] = dist[u] + uv;
+                        pathForward[v] = u;
+                        frontier.add(new VertexDist(v, dist[v]));
+                    }
+                }
+            }
+        }
+        // 'dist' contains the shortest distance from start to all nodes
+        return dist;
+    }
 }
